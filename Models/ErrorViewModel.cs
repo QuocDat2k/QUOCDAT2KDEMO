08d5fe3b441dd42336ee272ca610c9ea1c2dab63
quocdat2k <hellodat1401@gmail.com>
2021-11-11 03:33:51 -0800

    HELLO DAT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace QUOCDAT2KDEMO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
