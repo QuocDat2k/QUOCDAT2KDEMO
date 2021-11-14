26e67a135ccf0e7af7e9f34b3606c4408051819d
quocdat2k <hellodat1401@gmail.com>
2021-11-14 02:32:58 -0800

    1,2,3,4 Xong

---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcPerson.Models
+{
+    public class Person
+    {
+        public string? PersonId { get; set; }
+        public string? PersonName { get; set; }
+
+       
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+namespace MvcStudent.Models
+{
+    public class Student
+    {
+     
+        public string? StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public string? PhoneNumber { get; set; }
+
+    }
+}
